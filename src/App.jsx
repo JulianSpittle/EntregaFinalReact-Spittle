b19bbfb0aa1540cb4ecde145ca639cdf08b9f34b
Julian Spittle <julianspittle96@gmail.com>
2023-05-11 10:47:14 -0300

    tercer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
+import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container-fluid">
+      <Header/>
       <NavBar/>
       <ItemListContainer greeting={'Bienvenidos'}/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+// import imagenes
+
+const Header = () => {
+    return(
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-6">
+                    <img/>
+                </div>
+                <div className="col-md-6">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
