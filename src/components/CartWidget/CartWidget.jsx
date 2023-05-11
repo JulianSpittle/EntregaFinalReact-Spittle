20f8c45f589fa17a59ac6a70f6e9e27ec5fac92a
Julian Spittle <julianspittle96@gmail.com>
2023-05-11 13:21:50 -0300

    prettier

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,13 +1,12 @@
 const CartWidget = () => {
   return (
     <div className="bg-body-secondary p-3">
-<button type="button" class="btn btn-warning btn-lg position-relative">
-  <i className="fa fa-shopping-cart"/>
-  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-    0
-    <span class="visually-hidden">unread messages</span>
-  </span>
-</button>
+      <button type="button" class="btn btn-warning btn-lg position-relative">
+        <i className="fa fa-shopping-cart" />
+        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          0<span class="visually-hidden">unread messages</span>
+        </span>
+      </button>
     </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
-import LogoHeader from "./assets/logoheader.png"
+import LogoHeader from "./assets/logoheader.png";
 
 const Header = () => {
   return (
     <div className="container-fluid">
       <div className="row bg-body-secondary p-3">
-        <div className="col-md-6"><img src={LogoHeader} alt={"Logo Header"} width={48}/></div>
+        <div className="col-md-6">
+          <img src={LogoHeader} alt={"Logo Header"} width={48} />
+        </div>
         <div className="col-md-6 text-end">
           <a href="#" className="text-dark">
             Contactanos
