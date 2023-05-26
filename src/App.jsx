f606ecd8cc7692a3755dc0a1c7cb96c608e90d06
Julian Spittle <julianspittle96@gmail.com>
2023-05-26 20:44:23 -0300

    NavBar rework

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,25 @@ import "./App.css";
 import Error404 from './components/Error404';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import Main from './components/Main';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div className="App container-fluid">
+    <div>
       <BrowserRouter>
         <Header />
-        <NavBar />
-        <Routes>
-          <Route path={"/"} element={<ItemListContainer />}></Route>
-          <Route
-            path={"/Categoria/:id"}
-            element={<ItemListContainer />}
-          ></Route>
-          <Route path={"/Item/:id"} element={<ItemDetailContainer />}></Route>
-          <Route path={"/Main"} element={<Main />}></Route>
-          <Route path={"/*"} element={<Error404 />}></Route>
-        </Routes>
+        <NavBar/>
+          <Routes>
+            <Route path={"/"} element={<ItemListContainer />}></Route>
+            <Route path={"/Categoria/:id"} element={<ItemListContainer />}></Route>
+            <Route path={"/Item/:id"} element={<ItemDetailContainer />}></Route>
+            <Route path={"/Main"} element={<Main />}></Route>
+            <Route path={"/*"} element={<Error404 />}></Route>               
+          </Routes>
         <Footer />
       </BrowserRouter>
     </div>
